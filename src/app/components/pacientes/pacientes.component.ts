0c6f53c456272ca7a3de0c3a782e5eb862ae4560
lianvf <julianavargas567@gmail.com>
2025-05-15 10:21:47 -0300

    Mudanças para que outros componentes consigam utilizar os componentes hdk.

--- src/app/components/pacientes/pacientes.component.ts
+++ src/app/components/pacientes/pacientes.component.ts
@@ -3,11 +3,16 @@ import { MatTableDataSource } from '@angular/material/table';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Location } from '@angular/common';
 import { Usuario } from 'src/app/models/usuario.model';
+import { HdkDivisor } from '../hdk/divisor/hdk-divisor.component';
+import { TabelaComponent } from '../hdk/tabela/hdk-tabela.component';
+import { HdkButtonComponent } from '../hdk/button/hdk-button.component';
 
 @Component({
   selector: 'app-pacientes',
   templateUrl: './pacientes.component.html',
   styleUrls: ['./pacientes.component.scss'],
+  standalone: true,
+  imports: [HdkDivisor, TabelaComponent, HdkButtonComponent],
 })
 export class PacientesComponent implements OnInit {
   dataSource: MatTableDataSource<Usuario> = new MatTableDataSource<Usuario>([]);
