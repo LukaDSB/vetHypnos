92ab5f9c93de8e984ad50ba0dea9aa9fdfec2e67
lianvf <julianavargas567@gmail.com>
2025-05-23 12:31:58 -0300

    Integração de Animais, adição da versão 16.4.1 do ngx mask.

--- src/app/components/pacientes/pacientes.component.ts
+++ src/app/components/pacientes/pacientes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { UsuarioService } from 'src/app/services/usuario.service';
+import { AnimalService } from 'src/app/services/animal.service';
 import { Location } from '@angular/common';
-import { Usuario } from 'src/app/models/usuario.model';
+import {Animal} from 'src/app/models/animal.model';
 import { ModalPacientesComponent } from './modal-pacientes/modal-pacientes.component';
 import { HdkButtonComponent } from '../hdk/button/hdk-button.component';
 import { HdkDivisor } from '../hdk/divisor/hdk-divisor.component';
@@ -16,19 +16,45 @@ import { TabelaComponent } from '../hdk/tabela/hdk-tabela.component';
   imports: [ModalPacientesComponent, HdkButtonComponent, HdkDivisor, TabelaComponent],
 })
 export class PacientesComponent implements OnInit {
-  dataSource: MatTableDataSource<Usuario> = new MatTableDataSource<Usuario>([]);
-  displayedColumns: string[] = ['id', 'nome', 'email', 'especialidade', 'acoes'];
+  dataSource: MatTableDataSource<Animal> = new MatTableDataSource<Animal>([]);
+  displayedColumns: string[] = ['id', 'nome', 'especie_id', 'data_nascimento', 'tutor_id', 'peso', 'sexo', 'acoes'];
   @ViewChild('modalPacientes') modalPacientesComponent!: ModalPacientesComponent;
 
 
-  constructor(private usuarioService: UsuarioService, private location: Location) {}
+  constructor(private animalService: AnimalService, private location: Location) {}
   
   ngOnInit() {
     this.carregarDados();
   }
 
+  cadastrarAnimal(animal: Animal) {
+    console.log(animal);
+    this.animalService.cadastrarAnimal(animal).subscribe({
+      next: () => {
+        this.carregarDados();
+      },
+      error: (err) => {
+        console.error('Erro ao cadastrar animal:', err);
+      }
+    })
+  }
+
+  deletarAnimal(animal: Animal){
+    if (confirm(`Deseja realmente excluir "${animal.nome}"?`)){
+      this.animalService.deletarAnimal(animal.id).subscribe({
+        next: () => {
+          console.log(`Animal com ID ${animal.id} excluído.`);
+          this.carregarDados();
+        },
+        error: (err) => {
+          console.error(`Erro ao excluir animal de ${animal.id}`, err);
+        }
+      });
+    }
+  }
+
   carregarDados() {
-    this.usuarioService.getUsuarios().subscribe((data) => {
+     this.animalService.getAnimais().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
     });
   }
