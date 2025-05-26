9cdff4422a4eb64216e71faa3f5e89ed5c8ae81d
lianvf <julianavargas567@gmail.com>
2025-05-26 11:53:33 -0300

    SP05/09 - Integração da tabela de prontuários.

--- src/app/components/prontuarios/prontuarios.component.ts
+++ src/app/components/prontuarios/prontuarios.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
+import { Prontuario } from 'src/app/models/prontuario';
+import { ProntuarioService } from 'src/app/services/prontuario.service';
 import { TabelaComponent } from '../hdk/tabela/hdk-tabela.component';
 import { HdkButtonComponent } from '../hdk/button/hdk-button.component';
 import { HdkDivisor } from '../hdk/divisor/hdk-divisor.component';
 import { AssociarPacienteModalComponent } from './modalAssociarPaciente/associar-paciente-modal/associar-paciente-modal.component';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-prontuarios',
@@ -13,9 +16,46 @@ import { AssociarPacienteModalComponent } from './modalAssociarPaciente/associar
   standalone: true,
   imports: [HdkDivisor, HdkButtonComponent, TabelaComponent],
 })
-export class ProntuariosComponent {
+export class ProntuariosComponent implements OnInit {
+  dataSource:MatTableDataSource<Prontuario> = new MatTableDataSource<Prontuario>([]);
+  displayedColumns: string[] = ['id', 'nome_usuario','nome_animal', 'data_prontuario', 'procedimento', 'status', 'acoes'];
 
-  constructor(public dialog: MatDialog){}
+  constructor(public dialog: MatDialog, private prontuarioService: ProntuarioService){}
+
+ngOnInit(): void {
+  this.carregarDados();
+}
+
+criarProntuario(prontuario: Prontuario){
+  this.prontuarioService.criarProntuario(prontuario).subscribe({
+    next: () => {
+      this.carregarDados();
+    },
+    error: (err) => {
+      console.error('Erro ao criar prontuário: ', err);
+    }
+  })
+}
+
+deletarProntuario(prontuario: Prontuario){
+  if (confirm(`Deseja realmente excluir "${prontuario.id}"?`)){
+    this.prontuarioService.deletarProntuario(prontuario.id).subscribe({
+      next: () => {
+        console.log(`Prontuario com ID ${prontuario.id} excluído.`);
+        this.carregarDados();
+      },
+      error: (err) => {
+        console.error(`Erro ao excluir prontuario de ID ${prontuario.id}`, err);
+      }
+    });
+  }
+}
+
+carregarDados(){
+  this.prontuarioService.getProntuarios().subscribe((data) => {
+    this.dataSource = new MatTableDataSource(data);
+  })
+}
 
   abrirModal(): void {
     console.log('Modal abriu');
