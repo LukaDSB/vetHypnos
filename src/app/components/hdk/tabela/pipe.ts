3697b474d46acaaeba810fc51edeb2cc7cd74081
lianvf <julianavargas567@gmail.com>
2025-05-29 11:50:16 -0300

    Alterações feitas.

--- src/app/components/hdk/tabela/pipe.ts
+++ src/app/components/hdk/tabela/pipe.ts
@@ -8,10 +8,8 @@ export class DateMaskPipe implements PipeTransform {
   transform(value: string | number): string {
     if (!value) return 'Não informado';
 
-    // Remove tudo que não for número
     const digits = value.toString().replace(/\D/g, '');
 
-    // Aplica a máscara 00/00/0000 com regex
     const masked = digits.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
 
     return masked.length === 10 ? masked : 'Data inválida';
--- src/app/components/pacientes/modal-pacientes/modal-pacientes.component.ts
+++ src/app/components/pacientes/modal-pacientes/modal-pacientes.component.ts
@@ -4,8 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { HdkButtonComponent } from '../../hdk/button/hdk-button.component';
 import { Location } from '@angular/common';
-import {Animal} from 'src/app/models/animal.model';
-import { AnimalService } from 'src/app/services/animal.service';
+import { Animal } from 'src/app/models/animal.model';
 import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
@@ -13,36 +12,38 @@ import { NgxMaskDirective } from 'ngx-mask';
   templateUrl: './modal-pacientes.component.html',
   styleUrls: ['./modal-pacientes.component.scss'],
   standalone: true,
-  imports:[FormsModule, CommonModule, HdkButtonComponent, NgxMaskDirective]
+  imports: [FormsModule, CommonModule, HdkButtonComponent, NgxMaskDirective]
 })
-export class ModalPacientesComponent implements OnInit{
- isCadastroModalOpen = false;
-    nomeAnimal = "";
-    DataNascAnimal = 0;
-    SexoAnimal = "";
-    PesoAnimal = 0;
-    TutorAnimal = 0;
-    especieAnimal = 0;
-    especies: any[] = [];
-    @Output() cadastrar = new EventEmitter<Animal>();
+export class ModalPacientesComponent implements OnInit {
+  isCadastroModalOpen = false;
+  nomeAnimal = '';
+  DataNascAnimal = 0;
+  SexoAnimal = '';
+  PesoAnimal = 0;
+  TutorAnimal = 0;
+  especieAnimal = 1;
+  especies: any[] = [];
 
-  constructor(private authService: AuthService, private location: Location, private animalService: AnimalService) {}
-  
+  @Output() cadastrar = new EventEmitter<Animal>();
+
+  constructor(
+    private authService: AuthService,
+    private location: Location
+  ) {}
+
   ngOnInit(): void {
-      this.animalService.getAnimais().subscribe((animais: any[]) => {
-        this.especies = this.getEspeciesUnicas(animais);
-    });
+
   }
 
-  cadastrarAnimal(){
+  cadastrarAnimal() {
     const novoAnimal: Animal = {
-      id:0,
+      id: 0,
       nome: this.nomeAnimal,
       data_nascimento: this.DataNascAnimal,
       sexo: this.SexoAnimal,
       peso: this.PesoAnimal,
       obito: 0,
-      tutor_id:this.TutorAnimal,
+      tutor_id: this.TutorAnimal,
       especie_id: this.especieAnimal,
     };
 
@@ -50,18 +51,7 @@ export class ModalPacientesComponent implements OnInit{
     this.closeCadastro();
   }
 
-  getEspeciesUnicas(animais: any[]): any[] {
-    const especiesMap = new Map<number, any>();
-    for (const animal of animais) {
-      const especie = animal.especie;
-      if (especie && !especiesMap.has(especie.id)) {
-        especiesMap.set(especie.id, especie);
-      }
-    }
-    return Array.from(especiesMap.values());
-  }
-
-  voltar(){
+  voltar() {
     this.location.back();
   }
 
@@ -77,4 +67,3 @@ export class ModalPacientesComponent implements OnInit{
     this.authService.loginMock();
   }
 }
-
